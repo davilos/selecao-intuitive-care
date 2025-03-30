c1b4d308b3df8b3f1576a6753176974509064091
Dávilos <daviloscostagg@hotmail.com>
2025-03-30 18:44:02 -0300

    feat: implementa o componente da tabela

--- app/src/components/DataTable.vue
+++ app/src/components/DataTable.vue
@@ -1,9 +1,54 @@
 <template>
-  <div>
-    <h4>Teste</h4>
+  <div v-if="operadoras">
+    <v-data-table
+      v-model:items-per-page="itemsPerPage"
+      :headers="headers"
+      :items="data"
+      item-value="userID"
+      class="elevation-1"
+    >
+    </v-data-table>
   </div>
 </template>
 
 <script>
-export default {}
+import { VDataTable } from 'vuetify/components'
+import { getAllOperadoras } from '../services/getAllOperadoras'
+
+export default {
+  components: {
+    VDataTable,
+  },
+  setup() {
+    const { operadoras, error, load } = getAllOperadoras()
+
+    load()
+
+    return {
+      operadoras,
+      error,
+      load,
+      itemsPerPage: 10,
+      headers: [
+        {
+          key: 'userID',
+          title: 'User Id',
+          align: 'start',
+          sortable: false,
+        },
+        {
+          key: 'title',
+          title: 'Title',
+          align: 'start',
+        },
+        {
+          key: 'body',
+          title: 'Body',
+          align: 'start',
+        },
+      ],
+      data: operadoras,
+    }
+  },
+}
 </script>
