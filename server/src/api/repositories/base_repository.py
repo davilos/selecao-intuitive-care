4c378abb04f157b5da21fbe8fa6d4f44d826a4c8
Dávilos <daviloscostagg@hotmail.com>
2025-03-31 17:08:34 -0300

    refactor: remove o componente Modal e operações no backend

--- server/src/api/repositories/base_repository.py
+++ server/src/api/repositories/base_repository.py
@@ -1,10 +1,7 @@
 from abc import ABC, abstractmethod
 
-class BaseRepository(ABC):
-    @abstractmethod
-    def find_many(self):
-        pass
 
+class BaseRepository(ABC):
     @abstractmethod
     def find_all(self):
         pass
