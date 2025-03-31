27cb88d1361fee078df2dba14bae407990f93925
Dávilos <daviloscostagg@hotmail.com>
2025-03-31 19:33:35 -0300

    refactor: altera query para retornar as operadoras da área de saúde com mais gastos

--- server/src/api/repositories/operadoras_repository.py
+++ server/src/api/repositories/operadoras_repository.py
@@ -10,14 +10,22 @@ class MySQLOperadorasRepository(BaseRepository):
             user="root",
             password="dev",
             db="intuitive_care_db",
-            port=9000,
-            autocommit=True,
+            port=9000
         )
 
     def find_all(self):
         try:
             with self.db_config.cursor() as cursor:
                 query = """
+                    WITH aggregated_sales AS (
+                        SELECT 
+                            REG_ANS,
+                            SUM(VL_SALDO_FINAL) AS total_vendas
+                        FROM demonstracao_contabeis
+                        WHERE
+                        DESCRICAO LIKE '%EVENTOS/%SINISTROS CONHECIDOS%ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR%'
+                        GROUP BY REG_ANS
+                    )
                     SELECT 
                         o.Registro_ANS,
                         o.CNPJ,
@@ -37,21 +45,14 @@ class MySQLOperadorasRepository(BaseRepository):
                         o.Representante,
                         o.Cargo_Representante,
                         o.Regiao_de_Comercializacao,
-                        DATE_FORMAT(o.Data_Registro_ANS, '%d/%m/%Y'),
-                        SUM(dc.VL_SALDO_FINAL) as total_vendas
+                        DATE_FORMAT(o.Data_Registro_ANS, '%d/%m/%Y')
                     FROM operadoras o
-                    INNER JOIN demonstracao_contabeis dc 
-                        ON o.Registro_ANS = dc.REG_ANS
-                    WHERE dc.CD_CONTA_CONTABIL = '41'
-                    GROUP BY 
-                        o.Registro_ANS, o.CNPJ, o.Razao_Social, o.Nome_Fantasia, o.Modalidade,
-                        o.Logradouro, o.Numero, o.Complemento, o.Bairro, o.Cidade, o.UF, o.CEP,
-                        o.DDD, o.Telefone, o.Fax, o.Endereco_eletronico, o.Representante,
-                        o.Cargo_Representante, o.Regiao_de_Comercializacao, o.Data_Registro_ANS
-                    ORDER BY total_vendas DESC
-                    LIMIT 10;
+                    INNER JOIN aggregated_sales agg 
+                        ON o.Registro_ANS = agg.REG_ANS
+                    ORDER BY agg.total_vendas DESC
+                    LIMIT 50;
                 """
-
+                                        
                 cursor.execute(query)
                 return cursor.fetchall()
         except Exception as e:
