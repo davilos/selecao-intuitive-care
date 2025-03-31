5c94742781e33e0fded9d053a4ffad74e211e8b7
Dávilos <daviloscostagg@hotmail.com>
2025-03-31 17:10:30 -0300

    feat: implementa lógica para padronização dos dados

--- server/src/api/server.py
+++ server/src/api/server.py
@@ -1,4 +1,3 @@
-from services.fetch_many_operadoras import FetchManyOperadorasService
 from services.fetch_all_operadoras import FetchAllOperadorasService
 
 from flask import Flask, jsonify
@@ -7,60 +6,62 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 cors = CORS(app)
 
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["CORS_HEADERS"] = "Content-Type"
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'dev'
-app.config['MYSQL_DB'] = 'intuitive_care_db'
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "dev"
+app.config["MYSQL_DB"] = "intuitive_care_db"
+
 
 @app.route("/get-all-operadoras", methods=["GET"])
 @cross_origin()
 def get_all_operadoras():
-  try:
-    service = FetchAllOperadorasService()
+    try:
+        service = FetchAllOperadorasService()
 
-    operadoras = service.execute()
+        raw_operadoras = service.execute()
+        operadoras = []
 
-    response = {
-      'error': False,
-      'message': 'Operadoras carregadas com sucesso',
-      'data': operadoras
-    }
+        for op in raw_operadoras:
+            operadora = {
+                "registro_ans": op[0],
+                "cnpj": op[1],
+                "razao_social": op[2],
+                "nome_fantasia": op[3],
+                "modalidade": op[4],
+                "endereco": {
+                    "logradouro": op[5],
+                    "numero": op[6],
+                    "complemento": op[7],
+                    "bairro": op[8],
+                    "cidade": op[9],
+                    "uf": op[10],
+                    "cep": op[11],
+                },
+                "contato": {
+                   "telefone": op[12],
+                    "fax": op[13],
+                    "email": op[14],
+                },
+                "responsavel": {"nome": op[15], "cargo": op[16]},
+                "regiao_comercializacao": op[17],
+                "data_registro": op[18]
+            }
+            operadoras.append(operadora)
 
-    return jsonify(response), 200
+        response = {
+            "error": False,
+            "message": "Operadoras carregadas com sucesso",
+            "data": operadoras,
+        }
 
-  except Exception as e:
-    response = {
-      'error': True,
-      'message': str(e),
-      'data': None
-    }
-    return jsonify(response), 500
+        return jsonify(response), 200
 
-@app.route("/get-operadoras", methods=["GET"])
-@cross_origin()
-def get_operadoras():
-  try:
-    service = FetchManyOperadorasService()
+    except Exception as e:
+        response = {"error": True, "message": str(e), "data": None}
+        return jsonify(response), 500
 
-    operadoras = service.execute("teste")
 
-    response = {
-      'error': False,
-      'message': 'Operadoras carregadas com sucesso',
-      'data': operadoras
-    }
-
-    return jsonify(response), 200
-
-  except Exception as e:
-    response = {
-      'error': True,
-      'message': str(e),
-      'data': None
-    }
-    return jsonify(response), 500
-  
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True, port=5000)
